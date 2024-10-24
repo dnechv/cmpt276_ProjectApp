9dfbe899a73cf92ec948feae6fb2157b38863538
Dmitry Nechvolod <dnechv@MacBook-Pro-7.local>
2024-10-23 18:40:11 -0700

    Initial commit

---
+++ app/src/main/java/com/example/memoryconnect/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.memoryconnect;
+
+//main screen - start of the app
+
+//the screen shows patients list to caregiver
+//TODO - populate list as entries in data bases added
+//TODO - PIN
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+}
